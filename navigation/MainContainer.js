866b3e9462f8cfac311e6c3b6f162f5180cb5439
Peng Shen <sp100809@gmail.com>
2022-02-02 13:54:06 +1100

    just-bottom-tabs

---
+++ navigation/MainContainer.js
@@ -0,0 +1,46 @@
+import * as React from "react";
+import { View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from 'react-native-vector-icons/Ionicons';
+//screens
+import HomeScreen from "./screens/HomeScreen";
+import DetailsScreen from "./screens/DetailScreen";
+import SettingsScreen from "./screens/SettingScreen";
+
+//Screen names
+const homeName = "Home";
+const detailsName = "Details";
+const settingsName = "Settings";
+
+const Tab = createBottomTabNavigator();
+
+export default function MainContainer() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName={homeName}
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            let rn = route.name;
+
+            if (rn === homeName) {
+              iconName = focused ? "home" : "home-outline";
+            } else if (rn === detailsName) {
+              iconName = focused ? "list" : "list-outline";
+            } else if (rn === settingsName) {
+              iconName = focused ? "settings" : "settings-outline";
+            }
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        
+      >
+        <Tab.Screen name={homeName} component={HomeScreen} />
+        <Tab.Screen name={detailsName} component={DetailsScreen} />
+        <Tab.Screen name={settingsName} component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ navigation/screens/HomeScreen.js
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+export default function HomeScreen({navigation}) {
+    return (
+        <View
+            style={{
+                flex: 1, 
+                alignItems: 'center', 
+                justifyContent: 'center'
+            }}
+        >
+            <Text
+                onPress={() => alert('this is home')}
+                style={{
+                    fontSize: 26,
+                    fontWeight: 'bold'
+                }}
+            >
+                Home Screen
+            </Text>
+        </View>
+    );
+}
